9ed4f96057b58316b89e29e202dcdf788d817700
alisoon81 <svb1381@gmail.com>
2025-07-13 16:26:38 +0330

    Update fake_server.py

--- fake_server.py
+++ fake_server.py
@@ -1,10 +1,12 @@
 import socket
+import os
 
 def start_fake_server():
+    port = int(os.environ.get("PORT", 3000))  # پورت رو از ENV بگیر
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', 8080))  # پورت مورد نیاز Render
+    s.bind(("0.0.0.0", port))
     s.listen(1)
-    print("Fake server is running on port 8080...")
+    print(f"🚀 Fake server is listening on port {port}")
     while True:
         conn, addr = s.accept()
         conn.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nBot is running.")
